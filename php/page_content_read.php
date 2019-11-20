3e9d0e973048df979750633827f909f3222ef257
lilarcor27 <57851440+lilarcor27@users.noreply.github.com>
2019-11-20 17:03:18 +1300

    DB 연동으로 했는데 페이지 넘버 순으로 정렬해서 쿼리 보내야 함

--- php/page_content_read.php
+++ php/page_content_read.php
@@ -2,6 +2,10 @@
 
 include "db_info.php";
 
+define("FIRST_PAGE", 1000);
+define("COMMON_PAGE", 1001);
+define("HEADLINE_PAGE", 1002);
+
 if(!isset($_GET["pname"])) {
 	mysqli_close($conn);
 	die("something wrong: pname isnt set.");
@@ -10,7 +14,7 @@ $person = $_GET["pname"];
 
 $person = stringSanitaze($person, $conn);
 
-$sqlquery = "SELECT * FROM lilarcor27.page_content WHERE person='$person'";
+$sqlquery = "SELECT * FROM lilarcor27.page_content WHERE person='$person' ORDER BY 'page_num' DESC";
 $result_set = mysqli_query($conn, $sqlquery);
 $row = mysqli_fetch_array($result_set);
 
@@ -18,7 +22,15 @@ if(!$row) {
 	mysqli_close($conn);
 } else {
 	do {
-		if($row['page_num'] == 1) {
+		$typeOfThisPage = null;
+		if($row["page_information"][0] === 'f')
+			$typeOfThisPage = constant("FIRST_PAGE");
+		else if($row["page_information"][0] === 'c')
+			$typeOfThisPage = constant("COMMON_PAGE");
+		else if($row["page_information"][0] === 'h')
+			$typeOfThisPage = constant("HEADLINE_PAGE");
+		echo "<script>console.log($typeOfThisPage);</script>";
+		if($typeOfThisPage === constant("FIRST_PAGE")) {
 			$c1 = $row['content1'];
 			$c2 = $row['content2'];
 			$c3 = $row['content3'];
@@ -41,7 +53,7 @@ if(!$row) {
 					</div>
 				</div>
 			";
-		} else {
+		} else if($typeOfThisPage === constant("COMMON_PAGE")) {
 			$c1 = $row['content1'];
 			$c2 = $row['content2'];
 			$c3 = $row['content3'];
@@ -55,24 +67,35 @@ if(!$row) {
 			if($f1)
 				echo "
 					<img src='image/$person/$f1' width='100%'></img>
-					
+
 				";
 			echo "<h2>$c1</h2>";
-			
+
 			if($f2)
 				echo "
 					<img src='image/$person/$f2' width='100%'></img>
-					
+
 				";
 			echo "<h2>$c2</h2>";
-			
+
 			if($f3)
 				echo "
 					<img src='image/$person/$f3' width='100%'></img>
-					
+
 				";
 			echo "<h2>$c3</h2>";
 			echo "
+					</div>
+				</div>
+			";
+		} else if($typeOfThisPage === constant("HEADLINE_PAGE")) {
+			$c1 = $row['content1'];
+			echo "
+				<div class='page'>
+					<div class='content_wrapper'>
+						<div class='headline'>
+							$c1
+						</div>
 					</div>
 				</div>
 			";
@@ -80,15 +103,15 @@ if(!$row) {
 	} while($row = mysqli_fetch_array($result_set));
 }
 
-echo "
-	<div class='page'>
-		<div class='content_wrapper'>
-			<div class='headline'>
-				Coming soon
-			</div>
-		</div>
-	</div>
-";
+// echo "
+// 	<div class='page'>
+// 		<div class='content_wrapper'>
+// 			<div class='headline'>
+// 				Coming soon
+// 			</div>
+// 		</div>
+// 	</div>
+// ";
 
 mysqli_close($conn);
 
